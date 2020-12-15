c06bf7cc50337b0ca12230d93688395ba189f0af
BuildTools <unconfigured@null.spigotmc.org>
2020-12-16 00:35:15 +0100

    Initial commit

---
+++ src/main/java/de/berstanio/server/DSBSocket.java
@@ -0,0 +1,52 @@
+package de.berstanio.server;
+
+import de.berstanio.ghgparser.DSBNotLoadableException;
+import de.berstanio.ghgparser.GHGParser;
+import de.berstanio.ghgparser.User;
+
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class DSBSocket {
+
+    private ServerSocket socket;
+
+    public DSBSocket(InetAddress address, int port) throws IOException {
+        setSocket(new ServerSocket(port, 50, address));
+        new Thread(this::listen).start();
+    }
+
+    public void listen(){
+        try {
+            System.out.println("Listen to new client!");
+            Socket client = getSocket().accept();
+            new Thread(this::listen).start();
+            System.out.println(client.getInetAddress().getHostAddress() + " hat sich verbunden");
+            ObjectInputStream objectInputStream = new ObjectInputStream(client.getInputStream());
+            ObjectOutputStream objectOutputStream = new ObjectOutputStream(client.getOutputStream());
+            int week = objectInputStream.readInt();
+            User user = (User) objectInputStream.readObject();
+            String html = GHGParser.generateHtmlFile(user, PersonalDSBServer.getPlans(user.getYear()).get(week));
+            objectOutputStream.writeObject(html);
+            objectOutputStream.flush();
+
+            objectOutputStream.close();
+            objectInputStream.close();
+            client.close();
+        } catch (IOException | ClassNotFoundException | DSBNotLoadableException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public ServerSocket getSocket() {
+        return socket;
+    }
+
+    public void setSocket(ServerSocket socket) {
+        this.socket = socket;
+    }
+}
---
+++ src/main/java/de/berstanio/server/PersonalDSBServer.java
@@ -0,0 +1,63 @@
+package de.berstanio.server;
+
+import de.berstanio.ghgparser.DSBNotLoadableException;
+import de.berstanio.ghgparser.GHGParser;
+import de.berstanio.ghgparser.Plan;
+
+import java.io.File;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.Calendar;
+import java.util.HashMap;
+import java.util.Timer;
+import java.util.TimerTask;
+import java.util.concurrent.TimeUnit;
+
+public class PersonalDSBServer {
+
+    private static HashMap<Integer, Plan> plans11 = new HashMap<>();
+    private static HashMap<Integer, Plan> plans12 = new HashMap<>();
+
+    public static void main(String[] args) throws IOException, DSBNotLoadableException {
+        GHGParser.init(GHGParser.class.getResourceAsStream("/rawPage.htm"), new File("user"));
+        DSBSocket dsbSocket = new DSBSocket(InetAddress.getByName("62.75.210.82"), 21589);
+        Timer timer = new Timer();
+        timer.schedule(new TimerTask() {
+            @Override
+            public void run() {
+                try {
+                    boolean b = update();
+                    if (b){
+                        //Sende Nachricht an alle Clients
+                    }
+                } catch (DSBNotLoadableException e) {
+                    e.printStackTrace();
+                }
+            }
+        }, 500, TimeUnit.MINUTES.toMicros(5));
+    }
+
+    public static boolean update() throws DSBNotLoadableException {
+        Calendar calendar = Calendar.getInstance();
+        int week = calendar.get(Calendar.WEEK_OF_YEAR);
+
+        for (int i = 0; i < 2; i++) {
+            int year = 11 + i;
+            for (int j = 0; j < 2; j++) {
+                int tmpWeek = week + j;
+                Plan plan = new Plan(year, tmpWeek);
+                if (getPlans(year).containsKey(tmpWeek)){
+                    if (!plan.getLastUpdate().after(getPlans(year).get(tmpWeek).getLastUpdate())){
+                        return false;
+                    }
+                }
+                getPlans(year).put(tmpWeek, new Plan(year, tmpWeek));
+            }
+        }
+        return true;
+    }
+
+    public static HashMap<Integer, Plan> getPlans(int year) {
+        return year == 12 ? plans12 : plans11;
+    }
+}

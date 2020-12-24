9db3cc5d19f6e19518bf8739e126e09ea34c9d67
Berstanio <sebastian_grunwald@web.de>
2020-12-24 01:36:04 +0100

    HashMap<UUID, User> wird gespeichert und kann geladen werden.

--- src/main/java/de/berstanio/server/PersonalDSBServer.java
+++ src/main/java/de/berstanio/server/PersonalDSBServer.java
@@ -3,14 +3,14 @@ package de.berstanio.server;
 import de.berstanio.ghgparser.DSBNotLoadableException;
 import de.berstanio.ghgparser.GHGParser;
 import de.berstanio.ghgparser.Plan;
+import de.berstanio.ghgparser.User;
 
-import java.io.File;
-import java.io.IOException;
+import java.io.*;
 import java.net.InetAddress;
-import java.util.Calendar;
-import java.util.HashMap;
-import java.util.Timer;
-import java.util.TimerTask;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.nio.file.StandardCopyOption;
+import java.util.*;
 import java.util.concurrent.TimeUnit;
 
 public class PersonalDSBServer {
@@ -18,8 +18,10 @@ public class PersonalDSBServer {
     private static HashMap<Integer, Plan> plans11 = new HashMap<>();
     private static HashMap<Integer, Plan> plans12 = new HashMap<>();
     private static String freeRooms;
+    private static HashMap<UUID, User> users = new HashMap<>();
 
-    public static void main(String[] args) throws IOException, DSBNotLoadableException {
+    public static void main(String[] args) throws IOException, DSBNotLoadableException, ClassNotFoundException {
+        loadUsers();
         GHGParser.init(GHGParser.class.getResourceAsStream("/rawPage.htm"), new File("user"));
         DSBSocket dsbSocket = new DSBSocket(InetAddress.getByName("62.75.210.181"), 21589);
         dsbSocket.start();
@@ -27,6 +29,11 @@ public class PersonalDSBServer {
         timer.schedule(new TimerTask() {
             @Override
             public void run() {
+                try {
+                    saveUsers();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
                 try {
                     boolean b = update();
                     if (b){
@@ -39,6 +46,39 @@ public class PersonalDSBServer {
                 }
             }
         }, 500, TimeUnit.MINUTES.toMillis(5));
+    }
+
+    public static void saveUsers() throws IOException {
+        if (Files.exists(Paths.get("users.yml"))){
+            Files.move(Paths.get("users.yml"), Paths.get("usersTMP.yml"), StandardCopyOption.REPLACE_EXISTING);
+        }
+        try (ObjectOutputStream objectOutputStream = new ObjectOutputStream(new FileOutputStream("users.yml"))) {
+            objectOutputStream.writeObject(getUsers());
+        }catch (IOException e){
+            Files.move(Paths.get("usersTMP.yml"), Paths.get("users.yml"), StandardCopyOption.REPLACE_EXISTING);
+        }
+        Files.deleteIfExists(Paths.get("usersTMP.yml"));
+    }
+
+    public static void loadUsers() throws IOException, ClassNotFoundException {
+        if (Files.exists(Paths.get("users.yml"))){
+            try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("users.yml"))){
+                setUsers((HashMap<UUID, User>) objectInputStream.readObject());
+            } catch (IOException | ClassNotFoundException e) {
+                if (Files.exists(Paths.get("usersTMP.yml"))){
+                    try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("usersTMP.yml"))) {
+                        setUsers((HashMap<UUID, User>) objectInputStream.readObject());
+                    }catch (IOException | ClassNotFoundException e2){
+
+                    }
+                }
+                throw e;
+            }
+        }else if (Files.exists(Paths.get("usersTMP.yml"))){
+            try (ObjectInputStream objectInputStream = new ObjectInputStream(new FileInputStream("usersTMP.yml"))) {
+                setUsers((HashMap<UUID, User>) objectInputStream.readObject());
+            }
+        }
     }
 
     public static boolean update() throws DSBNotLoadableException {
@@ -77,5 +117,13 @@ public class PersonalDSBServer {
 
     public static void setFreeRooms(String freeRooms) {
         PersonalDSBServer.freeRooms = freeRooms;
+    }
+
+    public static HashMap<UUID, User> getUsers() {
+        return users;
+    }
+
+    public static void setUsers(HashMap<UUID, User> users) {
+        PersonalDSBServer.users = users;
     }
 }
